5d671f16909cad8a2b4e559553317b52a0b6d5c0
kuijianiao <2455448200@qq.com>
2021-12-13 23:52:26 +0800

    初始化项目版本

--- test1.c
+++ test1.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 int main()
 { 
-  int x,y,sum;
-  printf("input data:\n");
-  scanf("%d",&x);
-  printf("Intput data :\n");
-  scanf("%d",&y);
-  sum=x+y;
-  printf("sum=%d\n",sum);
+  print("helloword!");
   return 0;
 }
